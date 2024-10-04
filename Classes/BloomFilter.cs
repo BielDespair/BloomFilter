00318dcaa7ebc2ffc6633bb6bd50df16ca0add26
Gabriel Leonel <gabrielvista7@gmail.com>
2024-10-03 21:04:45 -0300

    BloomFilter Class Created

---
+++ Classes/BloomFilter.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BloomFilter.Services {
+    public class FB {
+        public int n;
+        public float p;
+        public int m;
+        public int k;
+        private BitArray bitarr;
+
+        public FB(int n, float p) {
+            this.n = n;
+            this.p = p;
+            this.m = (int)Math.Ceiling((n* Math.Log(p)) / Math.Log(1 / Math.Pow(2, Math.Log(2))));
+            this.k = Convert.ToInt16(Math.Round((double)m / n) * Math.Log(2));
+            GC.Collect();
+            long memoriaAntes = GC.GetTotalMemory(true);
+            this.bitarr = new BitArray(this.m, false);
+            long memoriaDepois = GC.GetTotalMemory(true);
+            long tamanho = memoriaDepois - memoriaAntes;
+            Console.WriteLine($"Tamanho estimado de MinhaClasse: {tamanho} bytes");
+        }
+
+
+        public void Add(string element) {
+            int[] positions = this.GetPositions(element);
+            foreach (int position in positions) {
+                this.bitarr[position] = true;
+            }
+        }
+
+        public bool Contains(string element) {
+            int[] positions = this.GetPositions(element);
+            foreach (int position in positions) {
+                if (!this.bitarr[position]) return false;
+            }
+            return true;
+        }
+        private int[] GetPositions(string s) {
+            int[] positions = new int[this.k];
+
+            for (int i = 0; i < this.k; i++) {
+                positions[i] = GetSHA256Integer(s + i) % this.m;
+            }
+            return positions;
+        }
+        
+        private int GetSHA256Integer(string element) {
+            using (SHA256 sha256 = SHA256.Create()) {
+                byte[] digest = sha256.ComputeHash(Encoding.UTF8.GetBytes(element));
+                int integer = Math.Abs(BitConverter.ToInt32(digest, 0));
+                return integer;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using BloomFilter.Services;
+
+class Program {
+    static void Main(string[] args) {
+
+        // Parameters
+        int n = 100000;
+        float p = 0.01f;
+
+        FB fb = new FB(n, p);
+        Console.WriteLine($"n = {fb.n}, p = {fb.p}, m = {fb.m}, k = {fb.k}");
+
+        // Testes
+        int fp = 0;
+        int[] contained = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            contained[i] = i;
+            fb.Add(i.ToString());
+        }
+
+        foreach (int element in contained) {
+            if (!fb.Contains(element.ToString())) {
+                Console.WriteLine($"Element {element} inserted was not detected");
+            }
+            if (fb.Contains((-1*element).ToString())) {
+                fp++;
+            }
+        }
+
+        decimal fp_rate = fp / (decimal)contained.Length;
+        Console.WriteLine($"False positives rate: {(float)Math.Round(fp_rate*100, 1)}%");
+    }
+}
